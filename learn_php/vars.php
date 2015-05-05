2c1fb03a11ddf612d85c780b44511d8e96105efd
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-05 17:24:56 +0100

    some more learning exercises

--- learn_php/vars.php
+++ learn_php/vars.php
@@ -5,5 +5,19 @@ $y = 10.5;
 
 echo $txt . "<br >";
 echo $x . " + " . $y . " = " . $x+$y . "<br>";
-echo "$x + $y = ($x+$y)"
+echo "$x + $y = ($x+$y)";
+		
+function localVarFunc(){
+	$x = 7;
+	echo "<p>Variable x inside function is: $x</p>";
+}
+localVarFunc();
+
+function globalVarFunc(){
+	global $x, $y;
+	$y = $x + $y;
+}
+globalVarFunc();
+echo $y;
+
 ?>
