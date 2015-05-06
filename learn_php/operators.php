582715580f5b652b50286d8e084440f6627fb535
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-06 23:22:26 +0100

    added comparison experiments

--- learn_php/operators.php
+++ learn_php/operators.php
@@ -1,28 +1,52 @@
 <?php
 $x = 5;
 $y = 2;
-
-// arithmetic
-function printAritmetic($a, $b, $result, $operator){
+function printOperation($a, $b, $result, $operator){
+	if($result === true) $result = "true";
+	else if($result === false) $result = "false";
 	echo "$a $operator $b = $result <br>";
 }
 
+// arithmetic
+echo "<h4>Aritmetic Operators</h4>";
+
 // TODO replace with an array/list etc
 $res = $x+$y;
-printAritmetic($x, $y, $res, "+");
+printOperation($x, $y, $res, "+");
 
 $res = $y-$x;
-printAritmetic($y, $x, $res, "-");
+printOperation($y, $x, $res, "-");
 
 $res = $x * $y;
-printAritmetic($y, $x, $res, "*");
+printOperation($y, $x, $res, "*");
 
 $res = $x / $y; 					// interesting, result is a float
-printAritmetic($x, $y, $res, "/");
+printOperation($x, $y, $res, "/");
 
 $res = $x % $y;
-printAritmetic($x, $y, $res, "%");
+printOperation($x, $y, $res, "%");
 
 $res = $x ** $y;					// new in php 5.6
-printAritmetic($x, $y, $res, "**"); 
+printOperation($x, $y, $res, "**"); 
+
+// comparison
+echo "<h4>Comparison Operators</h4>";
+$com = 5 == "5";
+printOperation(5, "\"5\"",$com, "=="); // interesting, echo boolean true prints "1"
+
+
+$com = 5 === "5";
+printOperation(5, "\"5\"",$com, "==="); // interesting, echo boolean false doesn't print
+
+$com = 5 != "5";
+printOperation(5, "\"5\"",$com, "!=");
+$com = 5 !== "5";
+printOperation(5, "\"5\"",$com, "!==");
+$com = 5 <> "5";
+printOperation(5, "\"5\"",$com, "<>");
+$com = 5 < 2;
+printOperation(5, 2, $com, "<");
+$com = 5 > 2;
+printOperation(5, 2, $com, ">");
+
 ?>
