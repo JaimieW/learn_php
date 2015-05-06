39f6360654083d4fe50fbd2084514787a8397e2f
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-06 22:44:09 +0100

    small constant experiments

---
+++ learn_php/constants.php
@@ -0,0 +1,13 @@
+<?php
+define("SURPRISE", "COCKFACE");
+echo SURPRISE . "<br>";
+
+define("HELP", "for more options call 555-3228", true);
+echo help ."<br>";
+
+define("MR_PLOW", "Call Mr Plow, thats my name.<br>That name again is Mr Plow");
+function printJingle(){
+	echo MR_PLOW;
+} 
+printJingle();
+?>
