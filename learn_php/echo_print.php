2d15f20f9bc11f8fb1376352251146c8751bc54c
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-06 21:47:54 +0100

    some echo and print experiments

---
+++ learn_php/echo_print.php
@@ -0,0 +1,15 @@
+<?php
+echo "<h2>PHP is a thing</h2>";
+echo "Hello PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+$txt1 = "learning";
+$txt2 = "SURPRISE";
+$x = 3;
+$y = 1;
+
+echo "<h2>$txt1</h2>";
+print "$txt2 COCKFACE <br>";
+echo $x+$y . "<br>";
+
+?>
