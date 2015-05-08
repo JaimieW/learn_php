341f143f5a6dc85cb51fd37600790c98f2823cc9
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-08 13:24:15 +0100

    added return value experiment with fibonacci

--- learn_php/functions.php
+++ learn_php/functions.php
@@ -7,5 +7,12 @@ function sayWord($txt = "surprise"){
 sayWord("hello");
 sayWord();
 
+function fib($n){
+	if($n === 1 || $n === 2) return 1;
+	else return fib($n-1)+fib($n-2);
+}
 
+for($i=1;$i<=20;$i++){
+	echo fib($i) . " ";
+}
 ?>
