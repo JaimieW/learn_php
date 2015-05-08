ad7b059b6b17aa372c60a6613471a0ae1ff9839f
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-08 12:39:09 +0100

    added switch

--- learn_php/conditionals.php
+++ learn_php/conditionals.php
@@ -21,4 +21,30 @@ if($t<"12"){
 } else {
 	println("good evening");
 }
+
+$s = date("s");
+switch($s%6){
+	case 0:
+		println("aaa");
+		break;
+	case 1:
+		println("bbb");
+		break;
+	case 2:
+		println("ccc");
+		break;
+	case 3:
+		println("ddd");
+		break;
+	case 4:
+		println("eee");
+		break;
+	case 5:
+		println("fff");
+		break;
+	default:
+		println("wtf?");
+		break;
+}
+
 ?>
