9c71ab11bfdb77ba1a897b2349820d2de97173f9
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-11 20:44:53 +0100

    added required field functionality

--- learn_php/forms/validationForm.php
+++ learn_php/forms/validationForm.php
@@ -6,13 +6,23 @@
 <body>
 <?php
 //define vars
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 if($_SERVER["REQUEST_METHOD"] === "POST"){
-	$name = test_input($_POST["name"]);
-	$email = test_input($_POST["email"]);
-	$gender = test_input($_POST["gender"]);
-	$comment = test_input($_POST["comment"]);
-	$website = test_input($_POST["website"]);
+	if(empty($_POST["name"])) $nameErr = "Name is required";
+	else 					  $name = test_input($_POST["name"]);
+	
+	if(empty($_POST["email"]))	$emailErr = "Email is required";
+	else 						$email = test_input($_POST["email"]);
+	
+	if(!empty($_POST["website"]))
+		$website = test_input($_POST["website"]);
+
+	if(!empty($_POST["comment"]))
+		$comment = test_input($_POST["comment"]);
+	
+	if(empty($_POST["gender"]))	$genderErr = "Gender is required";
+	else 						$gender = test_input($_POST["gender"]);
 }
 function test_input($data){
 	$data=trim($data);
@@ -23,12 +33,21 @@ function test_input($data){
 ?>
 <h2>PHP Form Validation</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	Name: <input type="text" name="name"/><br/>
-	E-mail: <input type="text" name="email"/><br/>
-	Website: <input type="text" name="website"/><br/>
-	Comment: <textarea name="comment" rows="5", cols="40"></textarea><br/>
+	Name: <input type="text" name="name"/>
+	<span class="error">* <?php echo $nameErr;?></span>
+	<br/><br/>
+	E-mail: <input type="text" name="email"/>
+	<span class="error">* <?php echo $emailErr;?></span>
+	<br/><br/>
+	Website: <input type="text" name="website"/>
+	<?php echo $websiteErr;?>
+	<br/><br/>
+	Comment: <textarea name="comment" rows="5", cols="40"></textarea>
+	<br/><br/>
 	Gender: <input type="radio" name="gender" value="female">Female 
-			<input type="radio" name="gender" value="male">Male<br/>
+			<input type="radio" name="gender" value="male">Male
+	<span class="error">* <?php echo $genderErr;?></span>
+	<br/><br/>
 	<input type="submit" name="submit"/>
 </form>
 <?php
