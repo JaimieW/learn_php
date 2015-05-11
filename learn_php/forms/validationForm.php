15e2011f3cca18eadcc21ee3197627b28618a245
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-11 21:02:32 +0100

    added logic to keep values in the form

--- learn_php/forms/validationForm.php
+++ learn_php/forms/validationForm.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 <title>PHP Form validation example</title>
+<style>
+.error {color: #FF0000;}
+</style>
 </head>
 <body>
 <?php
@@ -46,20 +49,24 @@ function test_input($data){
 }
 ?>
 <h2>PHP Form Validation</h2>
+<p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	Name: <input type="text" name="name"/>
+	Name: <input type="text" name="name" value="<?php echo $name;?>"/>
 	<span class="error">* <?php echo $nameErr;?></span>
 	<br/><br/>
-	E-mail: <input type="text" name="email"/>
+	E-mail: <input type="text" name="email" value="<?php echo $email;?>"/>
 	<span class="error">* <?php echo $emailErr;?></span>
 	<br/><br/>
-	Website: <input type="text" name="website"/>
+	Website: <input type="text" name="website" value="<?php echo $website;?>"/>
 	<?php echo $websiteErr;?>
 	<br/><br/>
-	Comment: <textarea name="comment" rows="5", cols="40"></textarea>
+	Comment: <textarea name="comment" rows="5", cols="40"><?php echo $comment;?></textarea>
 	<br/><br/>
-	Gender: <input type="radio" name="gender" value="female">Female 
-			<input type="radio" name="gender" value="male">Male
+	Gender: 
+	<input type="radio" name="gender" 
+	<?php if(isset($gender) && $gender == "female") echo "checked";?> value="female">Female 
+	<input type="radio" name="gender" 
+	<?php if(isset($gender) && $gender == "male") echo "checked";?> value="male">Male
 	<span class="error">* <?php echo $genderErr;?></span>
 	<br/><br/>
 	<input type="submit" name="submit"/>
