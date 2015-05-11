c2a51e34823c88d8690bf49f30d5e10d63b89644
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-11 19:42:13 +0100

    added sorting experiments

--- learn_php/arrays.php
+++ learn_php/arrays.php
@@ -18,4 +18,38 @@ foreach($ages as $name => $age){
 }
 
 // array ref: http://www.w3schools.com/php/php_ref_array.asp
+
+echo "<br><br><br<>";
+
+
+
+
+// sorting arrays
+$nums= array(1,4,3,8,6,9,0,2);
+print_r($nums);				println();
+
+sort($nums);
+print_r($nums);				println();
+
+rsort($nums);
+print_r($nums);				println();
+
+echo "<br><br>";
+
+print_r($ages);				println();
+
+asort($ages);
+print_r($ages);				println();
+
+arsort($ages);
+print_r($ages);				println();
+
+ksort($ages);
+print_r($ages);				println();
+
+krsort($ages);
+print_r($ages);				println();
+
+
+
 ?>
