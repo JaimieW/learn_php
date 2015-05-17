993dde2230b9946e4fa520494ecb5592ee508f14
Jaimie Whiteisde <jaimiewhiteside@hotmail.com>
2015-05-17 12:01:42 +0100

    added int validation

--- learn_php/filters.php
+++ learn_php/filters.php
@@ -1,10 +1,12 @@
 <?php
+// filter list
 echo "<table><tr><th>Name</th><th>ID</th>";
 foreach(filter_list() as $id =>$filter) {
 	echo "<tr><td>". $filter . "</td><td>" . filter_id($filter) . '</td></tr>';
 }
 echo "</table><hr>";
 
+// validate string
 $str = "<h1>hello world</h1>";
 echo "initial str: ".$str;
 $filteredStr = filter_var($str, FILTER_SANITIZE_STRING);
@@ -20,4 +22,19 @@ echo "santized magic quotes: ". $filteredStr . "<br>";
 $filteredStr = filter_var($str, FILTER_SANITIZE_STRIPPED);
 echo "santized stripped: ". $filteredStr . "<br>";
 echo "<hr>";
+
+// validate integer
+function printIsInt($var){
+	if(filter_var($var, FILTER_VALIDATE_INT) !== false){
+		echo "$var is an integer <br/>";
+	} else {
+		echo "$var is not an integer <br/>";
+	}
+}
+
+printIsInt(100);
+printIsInt(10.3);
+printIsInt('a');
+printIsInt(0);
+
 ?>
